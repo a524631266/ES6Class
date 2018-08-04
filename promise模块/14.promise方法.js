587857df67ddd919e0d7ef65b88a57cd3a58f61b
zhangll <524631266@qq.com>
2018-08-05 00:42:54 +0800

    添加promise常见的4种方法 all,race resolve reject

--- promise模块/14.promise方法.js
+++ promise模块/14.promise方法.js
@@ -9,8 +9,22 @@ let read = require('./12.readFile.js')
  */
 
 // Promise all
-read("promise模块/1.txt").then(data=>{
-    console.log("nooww",data);
+Promise.all([read("promise模块/1.txt"),read("promise模块/2.txt")]).then(
+    data=>{
+        console.log("all方法",data);
+    }
+)
+// Promise race 竞争,谁快调用谁
+Promise.race([read("promise模块/2.txt"),read("promise模块/1.txt")]).then(
+    data=>{
+        console.log("race方法",data);
+    }
+)
+// Promise resolve 直接调用正常态
+Promise.resolve("成功了").then(data=>{
+    console.log(data);
 })
-
-
+// Promise resolve 直接调用失败态
+Promise.reject("失败了").then(null,err=>{
+    console.log(err);
+})
