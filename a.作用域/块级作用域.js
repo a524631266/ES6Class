3d857d3e707eb82f97fc9e87f339ce79f464c053
zhangll <524631266@qq.com>
2018-10-27 17:21:39 +0800

    添加作用域的特殊例子

--- a.作用域/块级作用域.js
+++ a.作用域/块级作用域.js
@@ -1,3 +1,6 @@
+// 比较好的官网
+//https://www.tslang.cn/docs/handbook/variable-declarations.html
+
 //案例1 
 var tmp = new Date();
 
@@ -56,3 +59,47 @@ window.a // 1
 let b = 1;
 window.b // undefined
 
+// let 与var 的区别
+//  在
+for (var i = 0; i < 10; i++) {
+  setTimeout(function() { console.log(i); }, 100 * i); // 打印的是windwo中的i,因为,var 提升到上一个空间
+}
+
+for (let i = 0; i < 10; i++) {
+  setTimeout(function() { console.log(i); }, 100 * i);
+}
+console.log("打印的是",i)
+
+function ff(){
+  for (var i = 0; i < 10; i++) {
+    setTimeout(function() { console.log("打印的是",i); }, 100 * i); // 打印的是windwo中的i,因为,var 提升到上一个空间
+  } 
+}
+ff()
+console.log("i",i)
+
+
+// 块作用域
+// 当用let声明一个变量，它使用的是词法作用域或块作用域。 
+// 不同于使用 var声明的变量那样可以在包含它们的函数外访问，
+// 块作用域变量在包含它们的块或for/if循环之外(即大括号{})是不能访问的
+
+
+//1.if 块外无法访问
+function ff2(){
+  if(false){
+    let a =2
+  }
+  console.log(a) // a is not defined
+}
+ff2()
+//2. for块外无法访问
+function ff2(){
+  let data = [1,2]
+  for(let i of data){
+    console.log("内部i",i)
+  }
+  console.log("外部i",i)
+}
+ff2()
+//function 为无法访问
