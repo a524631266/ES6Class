39f47bb816efc4d1aa869e876dee344dfdc1322e
zhangll <524631266@qq.com>
2018-11-03 17:38:55 +0800

    await

--- promise进阶-async/1.1async与await.js
+++ promise进阶-async/1.1async与await.js
@@ -11,16 +11,28 @@ let waitp = (s)=>{
     )
 }
 
-
-let time = async function aa(){// async是异步的，不堵塞接下来的代码，提高效率，此时放在一个内置的模块中等待执行
-    let rsp = await waitp(2)
-    console.log("已经返回rsp")
-    return rsp
+// async是异步的，不堵塞接下来的代码，提高效率，此时放在一个内置的模块中等待执行
+let time = async function aa(){
+    let rsp = await waitp(2) // 1.await 一般返回promise，如果是数字返回数字的promise
+    console.log("time 已经返回rsp")// 2.在内部会堵塞这句话
+    return rsp // 3.如果没有return 默认 resolve（undefined）
 }
 time().then(
     data=>{
-        console.log("async return ",data)
+        console.log("time async return ",data)
     }
 )
+
+let time2 = async function aa(){
+    let rsp = await 1 // 1.await 一般返回promise，如果是数字返回数字的promise
+    console.log("time2 返回数字proimse")// 2.在内部会堵塞这句话
+    return rsp // 
+}
+time2().then(
+    data=>{
+        console.log("time2 async return ",data)//
+    }
+)
+
 // 此句不会堵塞，立马执行
 console.log("马上执行！！！！！")
